01321980528d3dc4850f53686a119c017e69a217
futantan <luckytantanfu@gmail.com>
2016-03-12 12:57:27 +0800

    change ChildViewNames struct to ChildViewName enum

--- GankIO/TabBar/TabBarController.swift
+++ GankIO/TabBar/TabBarController.swift
@@ -10,37 +10,38 @@ import UIKit
 
 class TabBarController: UITabBarController
 {
-  private struct ChildViewNames {
-    static let girlsView = "Girls"
-    static let iOSView = "IOS"
-    static let androidView = "Android"
-    static let frontEndView = "FrontEnd"
+  private enum ChildViewName: String {
+    case GirlsView = "Girls"
+    case iOSView = "IOS"
+    case AndroidView = "Android"
+    case FrontEndView = "FrontEnd"
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     setAllChildViews()
     self.tabBar.barTintColor = UIColor(red:0.29, green:0.35, blue:0.5, alpha:1)
     self.tabBar.tintColor = UIColor.whiteColor()
   }
   
   func setAllChildViews() {
-    let girlsViewController = loadViewControllerFromStoryboard(ChildViewNames.girlsView)
+    let girlsViewController = loadViewControllerFromStoryboard(.GirlsView)
     girlsViewController!.tabBarItem.title = "福利"
     girlsViewController!.tabBarItem.image = UIImage(named: "bra")
     self.addChildViewController(girlsViewController!)
     
-    let iOSViewController = loadViewControllerFromStoryboard(ChildViewNames.iOSView) as! UINavigationController
+    let iOSViewController = loadViewControllerFromStoryboard(.iOSView) as! UINavigationController
     iOSViewController.tabBarItem.title = "iOS"
     iOSViewController.tabBarItem.image = UIImage(named: "apple")
     self.addChildViewController(iOSViewController)
     
-    let androidViewController = loadViewControllerFromStoryboard(ChildViewNames.androidView)
+    let androidViewController = loadViewControllerFromStoryboard(.AndroidView)
     androidViewController!.tabBarItem.title = "Android"
     androidViewController!.tabBarItem.image = UIImage(named: "android")
     self.addChildViewController(androidViewController!)
     
-    let frontEndViewController = loadViewControllerFromStoryboard(ChildViewNames.frontEndView)
+    let frontEndViewController = loadViewControllerFromStoryboard(.FrontEndView)
     frontEndViewController!.tabBarItem.title = "前端"
     frontEndViewController!.tabBarItem.image = UIImage(named: "frontEnd")
     self.addChildViewController(frontEndViewController!)
@@ -50,8 +51,8 @@ class TabBarController: UITabBarController
     
   }
   
-  func loadViewControllerFromStoryboard(name: String) -> UIViewController? {
-    let storyboard = UIStoryboard(name: name, bundle: nil)
+  private func loadViewControllerFromStoryboard(name: ChildViewName) -> UIViewController? {
+    let storyboard = UIStoryboard(name: name.rawValue, bundle: nil)
     let controller = storyboard.instantiateInitialViewController()
     return controller
   }
